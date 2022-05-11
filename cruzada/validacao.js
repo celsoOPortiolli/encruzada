11a728e8ef8f51749c2a9fa5e3b1f3e24cb95296
samusenna <samuel.sena@sptech.school>
2022-05-11 10:37:23 -0300

    implementação da validação.js

---
+++ cruzada/validacao.js
@@ -0,0 +1,97 @@
+     function avaliar() {
+
+        if (p1-m.value == M) {
+
+        } else if (p1-e.value == E) {
+
+        } else if (p1-m2.value == M) {
+
+        } else if (p1-o.value == O) {
+
+        } else if (p1-r.value == R) {
+
+        } else if (p1-i.value == I) {
+
+        } else if (p1-a.value == A) {
+
+        } else if (condition) {
+
+        } else if (condition) {
+
+        } else if (condition) {
+
+        } else if (condition) {
+
+        } else if (condition) {
+
+        } else if (condition) {
+
+        } else if (condition) {
+
+        } else if (condition) {
+
+        } else if (condition) {
+
+        } else if (condition) {
+
+        } else if (condition) {
+
+        } else if (condition) {
+
+        } else if (condition) {
+
+        } else if (condition) {
+
+        } else if (condition) {
+
+        } else if (condition) {
+
+        } else {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        } {
+
+        }
+
+    }
